1a08df0139f96b5835923dd3dff4d8ee69594f57
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2024-01-03 10:49:50 +0545

    if ,elseif,switch and for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'blog';
-}
+  disable:boolean=false;
+  name:string='bishal'
+  count:number=0;
+  data:string="";
+  getData(val:string){
+    console.log(val)
+    this.data=val;
+  }
+  increment(){
+    this.count++;
+    this.count++;
+  }
+  decrement(){
+    this.count--;
+  }
+  counter(type:string){
+    type==='add'?this.count++:this.count--
+  }
+  }
+
